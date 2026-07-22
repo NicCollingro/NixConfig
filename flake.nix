2ec557c25252527627c9f9f40c0a00a784ea2c78
Nic Collingro <nic@collingro.de>
2026-07-23 00:21:32 +0200

    Jeder macht mal Fehler, vor allem Jannik

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Ni(x)cOS dotfiles";
 
   outputs = { self, nixpkgs, ...}: {
-    nixosConfigurations.Kopernikus = {
+    nixosConfigurations.Kopernikus = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
         ./modules/configuration.nix
