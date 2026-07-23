36652b4fc78667be28c39c7be335ec9cb021b370
Nic Collingro <nic@collingro.de>
2026-07-23 21:33:45 +0200

    Next step in evolution

--- modules/User/hyprland.nix
+++ modules/User/hyprland.nix
@@ -5,30 +5,29 @@
 
   programs.kitty.enable = true;
 
-  {
-    wayland.windowManager.hyprland.settings = {
-      "$mod" = "SUPER";
-      bind =
-        [
-          "$mod, F, exec, firefox"
-          "$mod, Enter, exec, kitty"
-          "$mod, Q, killactive"
-          "$mod, L, exit"
-        ]
-        ++ (
-          # workspaces
-          # binds $mod + [shift +] {1..9} to [move to] workspace {1..9}
-          builtins.concatLists (builtins.genList (i:
-              let ws = i + 1;
-              in [
-                "$mod, code:1${toString i}, workspace, ${toString ws}"
-                "$mod SHIFT, code:1${toString i}, movetoworkspace, ${toString ws}"
-              ]
-            )
-            9)
-        );
-    };
-  }
+  wayland.windowManager.hyprland.settings = {
+    "$mod" = "SUPER";
+    bind =
+      [
+        "$mod, F, exec, firefox"
+        "$mod, Enter, exec, kitty"
+        "$mod, Q, killactive"
+        "$mod, L, exit"
+      ]
+      ++ (
+        # workspaces
+        #           # binds $mod + [shift +] {1..9} to [move to] workspace {1..9}
+        builtins.concatLists (builtins.genList (i:
+            let ws = i + 1;
+            in [
+              "$mod, code:1${toString i}, workspace, ${toString ws}"
+              "$mod SHIFT, code:1${toString i}, movetoworkspace, ${toString ws}"
+            ]
+          )
+          9)
+      );
+  };
+
 
   home.sessionVariables.NIXOS_OZONE_WL = "1";
 }
