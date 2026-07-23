fc586457210d888023d03e653c587c11ea759e38
Nic Collingro <nic@collingro.de>
2026-07-23 21:32:41 +0200

    Next step in evolution

--- modules/User/hyprland.nix
+++ modules/User/hyprland.nix
@@ -1,32 +1,34 @@
 { pkgs, ... }:
 {
   home.packages = with pkgs; [
-    waybar
-    wofi
-    polkit_gnome
   ];
 
   programs.kitty.enable = true;
 
-  wayland.windowManager.hyprland = {
-    enable = true;
-    xwayland.enable = true;
-    settings = {
-      monitor = ",preferred,auto,1";
+  {
+    wayland.windowManager.hyprland.settings = {
       "$mod" = "SUPER";
-      bind = [
-        "$mod, Q, exec, kitty"
-        "$mod, C, killactive"
-        "$mod, M, exit"
-        "$mod, E, exec, wofi --show drun"
-        "$mod, V, togglefloating"
-      ];
-      exec-once = [
-        "waybar"
-        "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1"
-      ];
+      bind =
+        [
+          "$mod, F, exec, firefox"
+          "$mod, Enter, exec, kitty"
+          "$mod, Q, killactive"
+          "$mod, L, exit"
+        ]
+        ++ (
+          # workspaces
+          # binds $mod + [shift +] {1..9} to [move to] workspace {1..9}
+          builtins.concatLists (builtins.genList (i:
+              let ws = i + 1;
+              in [
+                "$mod, code:1${toString i}, workspace, ${toString ws}"
+                "$mod SHIFT, code:1${toString i}, movetoworkspace, ${toString ws}"
+              ]
+            )
+            9)
+        );
     };
-  };
+  }
 
   home.sessionVariables.NIXOS_OZONE_WL = "1";
 }
